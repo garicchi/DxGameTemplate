fd80734059431d9d0e0c7a9083ee38ae931e1d36
garicchi <xgaricchi@gmail.com>
2014-12-11 10:52:47 +0900

    Projectテンプレート追加

---
+++ Templates/DXToolkit_Standard_WindowsPhone/TOOLKITHELPER/ScreenManager.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+
+#include "ScreenManager.h"
+
+
+using namespace std;
+using namespace DX;
+using namespace ToolkitHelper;
+
+ScreenManager::ScreenManager(const shared_ptr<DeviceResources>& deviceResources,ScreenBase* currentScreen)
+	:m_deviceResources(deviceResources),m_currentScreen(currentScreen){
+	
+}
+
+void ScreenManager::CreateResources(){
+	m_currentScreen->CreateResources();
+}
+
+void ScreenManager::ReleaseResources(){
+	m_currentScreen->ReleaseResources();
+	delete m_currentScreen;
+}
+
+void ScreenManager::Update(const StepTimer& timer,const GameInput& input){
+	auto nextScreen = m_currentScreen->Update(timer,input);
+	
+	if (nextScreen != m_currentScreen){
+		m_currentScreen->ReleaseResources();
+		delete m_currentScreen;
+		m_currentScreen=nextScreen;
+		m_currentScreen->CreateResources();
+		
+	}
+}
+
+void ScreenManager::Render(){
+	m_currentScreen->Render();
+}
